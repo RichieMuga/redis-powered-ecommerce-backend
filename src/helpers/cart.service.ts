320c6c08a25ed573ea427b1084a5429a6606ab32
Richie Mugambi <richiemugambi4@gmail.com>
2024-06-12 01:30:05 +0300

    feat: add cart service
    This is mainly for handling requests to the url of the domain one is to query

---
+++ src/helpers/cart.service.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export async function fetchUserCart(userId: string) {
+  const response = await axios.get(
+    `https://fakestoreapi.com/carts/user/${userId}`,
+  );
+  return response.data;
+}
+
+export async function updateCartById(cartId: string, cartData: object) {
+  const response = await axios.get(
+    `https://fakestoreapi.com/carts/${cartId}`,
+    cartData,
+  );
+  return response.data;
+}
+
+export const deleteCartById = async (id: string) => {
+  const response = await axios.delete(
+    `https://fakestoreapi.com/products/${id}`,
+  );
+  return response.data;
+};
+
+export const addNewCart = async (newData: object) => {
+  const response = await axios.post(
+    "https://fakestoreapi.com/products",
+    newData,
+  );
+  return response.data;
+};
