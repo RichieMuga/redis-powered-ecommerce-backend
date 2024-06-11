02ed76c977b15224448245bc04983aafc2061afd
Richie Mugambi <richiemugambi4@gmail.com>
2024-06-12 01:35:38 +0300

    feat: add entrypoint to application with necessary routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
 import { serverListener, app } from "./config/settings";
 
 import baseRouter from "./routes/base.routes";
+import productRouter from "./routes/product.routes";
+import categoryRouter from "./routes/category.routes";
+import cartRouter from "./routes/cart.routes";
+import userRouter from "./routes/user.routes";
+import {
+  preloadProductsCache,
+  preloadCategoriesCache,
+  preloadProductsInCategoryCache,
+} from "./helpers/cache/cachePreloader";
 
+// test api
 app.use("/api/v1", baseRouter);
+
+// preloaded data from redis cache
+preloadProductsCache();
+preloadCategoriesCache();
+preloadProductsInCategoryCache("electronics");
+
+app.use("/api/v1", productRouter);
+app.use("/api/v1", categoryRouter);
+app.use("/api/v1", cartRouter);
+app.use("/api/v1", userRouter);
 
 serverListener();
