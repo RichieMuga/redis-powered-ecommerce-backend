360e3f140b2cbba164c1a2a8954d8082b4dc6cce
Richie Mugambi <richiemugambi4@gmail.com>
2024-06-13 17:24:48 +0300

    feat: add index page documentation link

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { serverListener, app } from "./config/settings";
+import { Request, Response } from "express";
 
 import baseRouter from "./routes/base.routes";
 import productRouter from "./routes/product.routes";
@@ -12,6 +13,10 @@ import {
 } from "./helpers/cache/cachePreloader";
 
 // TODO: Version individual respective routes if changes occur
+app.get('/', (req: Request, res:Response) => {
+  res.send('<h1>Congratulations! server is running!</h1><h2>Redis Powered Ecommerce Application API</h2><a href="https://redis-powered-ecommerce-backend-docum.netlify.app/">Documentation</a>');
+});
+
 // api routes
 app.use("/", baseRouter);
 app.use("/api/v1", productRouter);
