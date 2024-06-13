6d935507cecf797829349896700ac60536e400e7
Richie Mugambi <richiemugambi4@gmail.com>
2024-06-13 09:07:14 +0300

    chore: organize cache to come after routes are mounted

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,18 @@ import {
   preloadProductsInCategoryCache,
 } from "./helpers/cache/cachePreloader";
 
-// test api
+// TODO: Version individual respective routes if changes occur
+// api routes
 app.use("/api/v1", baseRouter);
+app.use("/api/v1", productRouter);
+app.use("/api/v1", categoryRouter);
+app.use("/api/v1", cartRouter);
+app.use("/api/v1", userRouter);
 
 // preloaded data from redis cache
 preloadProductsCache();
 preloadCategoriesCache();
 preloadProductsInCategoryCache("electronics");
 
-app.use("/api/v1", productRouter);
-app.use("/api/v1", categoryRouter);
-app.use("/api/v1", cartRouter);
-app.use("/api/v1", userRouter);
 
 serverListener();
